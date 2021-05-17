591c18e23ab05db63d869c32890636f837f589df
John <johnparrett7@gmail.com>
2021-05-17 15:29:34 -0400

    first commit

---
+++ vue.js
@@ -0,0 +1,9 @@
+const shows = new Vue ({
+    el: '#show',
+    data: {
+        date: ['More Dates to Come!', 'May 29th, 2021', 'June 20th, 2021', 'September 11th, 2021', 'September 12th, 2021', 'September 25th, 2021'],
+        time: ['More Times to Come!', '6 pm - 8 pm', '4 pm - 6pm'],
+        location: ['More Locations to Come!', 'Bridge Park - Dublin, Ohio', 'Brewdog - Franklinton, Ohio']
+    },
+}); 
+

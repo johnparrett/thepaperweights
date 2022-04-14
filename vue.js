bb10015d939cc873754e0efa4bb584effe981442
John Parrett <john@johnparrett.com>
2022-04-14 16:14:45 -0400

    removed vue, added new shows and cleaned up code base a little

--- vue.js
+++
@@ -1,7 +0,0 @@
-const shows = new Vue ({
-    el: '#show',
-    data: {
-        time: ['More Times to Come!', '4 pm - 6 pm', '6 pm - 8 pm'],
-        location: ['More Locations to Come!', 'Bridge Park - Dublin, Ohio', 'Brewdog - Franklinton, Ohio'],
-    },
-}); 
